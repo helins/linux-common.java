6df9f9eebb22a7845955d52edd7dd7f8ebf09321
Adam Helinski <adam@helins.io>
2021-03-19 09:01:10 +0100

    Add missing package info file

--- src/main/java/io/helins/linux/package-info.java
+++ src/main/java/io/helins/linux/package-info.java
@@ -6,9 +6,6 @@
 
 
 /**
- * This package provides generic utilities related to Linux and JNA meant to be useful for building
- * more specific tooling.
- *
- * @see <a href=https://github.com/java-native-access/jna>Java Native Access</a>
+ * Miscellaneous JNA utilities related to Linux.
  */
 package io.helins.linux ;
